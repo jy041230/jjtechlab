c41ab8d7100533449d5d21c081d3512379db705b
jy041230 <86292947+jy041230@users.noreply.github.com>
2026-06-07 02:10:48 +0900

    Update sheetData.js

--- src/utils/sheetData.js
+++ src/utils/sheetData.js
@@ -1,7 +1,7 @@
 const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbyURpd6OPBf_goR0cCW4zWZ2tEoiiOc3RL1h6t1_NZIyUmCZU3YfB81MBu7SmsqxhFqKg/exec'
 
 export async function fetchSheetData() {
-  const response = await fetch(SHEET_API_URL)
+  const response = await fetch(`${SHEET_API_URL}?t=${Date.now()}`, { cache: 'no-store' })
   const data = await response.json()
   return data
 }
